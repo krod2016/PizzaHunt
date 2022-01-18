3a152c48ffd3b0dc427522cfb88dde305fbc100b
krod2016 <katrinajrodriguez1993@gmail.com>
2022-01-17 18:55:54 -0600

    added files, able to make and delete comments

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 const Pizza = require('./Pizza');
-const mongoose = require('mongoose');
+const Comment = require('./Comment');
 
-module.exports = {  Pizza };
+module.exports = { Pizza, Comment };
--- routes/api/index.js
+++ routes/api/index.js
@@ -7,6 +7,14 @@ const htmlRoutes = require('./html/html-routes');
 router.use('/api', apiRoutes);
 router.use('/', htmlRoutes);
 
+//import api routes from api/index
+const commentRoutes = require('./comment-routes');
+const pizzaRoutes = require('./pizza-routes');
+
+// add prefix of `/api` to all of the api routes imported from the `api` directory
+router.use('/comments', commentRoutes);
+router.use('/pizzas', pizzaRoutes);
+
 router.use((req, res) => {
   res.status(404).send('<h1>😝 404 Error!</h1>');
 });
